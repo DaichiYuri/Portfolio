7b7f557e9f0923061e8cdc29f8daea036d8940f3
Mahi <developer.luffy2024@gmail.com>
2025-04-15 21:40:33 +0530

    Added Google Analytics Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,16 @@ export default function RootLayout({
       <head>
       <link href="https://fonts.googleapis.com/css2?family=Arbutus+Slab&family=Bebas+Neue&family=Berkshire+Swash&family=Oswald:wght@200..700&family=Pacifico&display=swap&family=Sacramento&display=swap&family=Raleway&display=swap&family=Merienda:wght@300..900&display=swap&family=Lobster&display=swap&family=Unbounded&display=swap&family=Great+Vibes&family=Playfair+Display&display=swap" 
       rel="stylesheet"/>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-22Y15GT22M"></Script>
+      <Script>
+        {
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-22Y15GT22M');`
+        }
+      </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden overscroll-x-none text-white bg-[#121212]`}
